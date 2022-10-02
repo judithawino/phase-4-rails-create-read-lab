59eec4cfe5ed6a85c5fc90fc3f4c2a6240afb5df
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-02 22:00:00 +0300

    Completed

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plants = Plant.find_by(id: params[:id])
+        render json: plants
+    end
+
+    def create
+        plants = Plant.create(plants_params)
+        render json: plants, status: :created
+    end
+
+    private
+    def plants_params
+        params.permit(:name, :image, :price)
+    end
+end
